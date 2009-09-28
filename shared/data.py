3bce4236b569b0f035785bb148bc6d8ddc93bae4
Erik Eloff <eriel743@student.liu.se>
2009-09-28 15:54:42 +0200

    Added classes Unit, Obstacle and POI in data

--- shared/data.py
+++ shared/data.py
@@ -3,3 +3,26 @@ class MapObjectData(object):
     coord = None
     name = None
     timestamp = None
+
+    def __init__(self, coord, name, timestamp):
+        self.coord = coord
+        self.name = name
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return "<%s: %s, %s, %s>" % (self.__class__.__name__, self.coord, self.name, self.timestamp)
+
+class UnitData(MapObjectData):
+    pass
+
+class ObstacleData(MapObjectData):
+    pass
+
+class POIData(MapObjectData):
+    pass
+
+if __name__ == "__main__":
+    print MapObjectData((15.5726, 58.4035), 'MapObject', 0)
+    print UnitData((15.5726, 58.4035), 'MapObject', 0)
+    print ObstacleData((15.5726, 58.4035), 'MapObject', 0)
+    print POIData((15.5726, 58.4035), 'MapObject', 0)
