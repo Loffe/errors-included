e7ae03779e9c4a887fffa9447e1b502d1d750cdf
FreddieBoi <frepe593@student.liu.se>
2009-10-01 11:52:37 +0200

    developed mapgui much further. modded mapdata and map_xml just a bit too

--- client/map/map_xml_reader.py
+++ client/map/map_xml_reader.py
@@ -1,18 +1,32 @@
 # -*- coding: utf-8 -*-
 from xml.dom import minidom
-import data_storage
+import mapdata
 
 class MapXML:
-    __name = None
-    __levels = {}
-    __path = None
+    name = None
+    levels = {}
+    path = None
+    
+    def __init__(self, filename):
+        xmldoc = minidom.parse(open(filename)).documentElement
+
+        # <map>
+        for node in xmldoc.childNodes:
+            if node.nodeType != node.TEXT_NODE:
+                # <name />
+                if node.tagName == "name":
+                    self.handle_name_node(node)
+                # <area />
+                elif node.tagName == "area":
+                    self.handle_area_node(node)
+        # </map>
 
     def handle_name_node(self, node):
-        self.__name = node.attributes['id'].value
+        self.name = node.attributes['id'].value
 
     def handle_area_node(self, node):
         tiles = self.create_tiles(node.attributes['path'].value)
-        self.__levels[int(node.attributes['level'].value)] = tiles
+        self.levels[int(node.attributes['level'].value)] = tiles
 
     def handle_base_node(self, node):
         return [node.attributes['width'].value,
@@ -29,7 +43,7 @@ class MapXML:
                   "max_longitude":float(node.attributes['maxlon'].value)}
 
         return [int(node.attributes['id'].value),
-                self.__path + node.attributes['file'].value,
+                self.path + node.attributes['file'].value,
                 bounds,
                 node.attributes['type'].value]
 
@@ -42,14 +56,14 @@ class MapXML:
 
         tiles = None
         xmldoc = minidom.parse(open(path)).documentElement
-        self.__path = path[0:path.rfind("/") + 1]
+        self.path = path[0:path.rfind("/") + 1]
 
         for node in xmldoc.childNodes:
             if node.nodeType != node.TEXT_NODE:
                 # <base />
                 if node.tagName == "base":
                     width, height = self.handle_base_node(node)
-                    tiles = data_storage.Tiles(width, height)
+                    tiles = mapdata.Tiles(width, height)
                 # <size />
                 elif node.tagName == "size":
                     cols, rows = self.handle_size_node(node)
@@ -58,31 +72,8 @@ class MapXML:
                 elif node.tagName == "tile":
                     id, file, bounds, type = self.handle_tile_node(node)
                     if tiles:
-                        tiles.add_tile(data_storage.MapTile(id,
-                                                            file,
-                                                            bounds,
-                                                            type))
+                        tiles.add_tile(mapdata.Tile(id, file, bounds, type))
                     else:
                         print "<base /> must come before all <tile /> in the xml!"
                         exit()
         return tiles
-
-    def get_name(self):
-        return self.__name
-
-    def get_levels(self):
-        return self.__levels
-
-    def __init__(self, filename):
-        xmldoc = minidom.parse(open(filename)).documentElement
-
-        # <map>
-        for node in xmldoc.childNodes:
-            if node.nodeType != node.TEXT_NODE:
-                # <name />
-                if node.tagName == "name":
-                    self.handle_name_node(node)
-                # <area />
-                elif node.tagName == "area":
-                    self.handle_area_node(node)
-        # </map>
