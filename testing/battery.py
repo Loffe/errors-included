854462f803108371b9d9f1a00b5d03f6bf470a58
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-10-27 13:57:40 +0100

    updated battery info

--- testing/battery.py
+++ testing/battery.py
@@ -8,11 +8,12 @@ dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
  
 print 'charge level percentage',\
     dev_obj.GetProperty('battery.charge_level.percentage')
-#print 'charge current', dev_obj.GetProperty('battery.reporting.current')
-#print 'charge design', dev_obj.GetProperty('battery.reporting.design')
-#print 'charge last full',\dev_obj.GetProperty('battery.reporting.last_full')
-#print 'charge unit', dev_obj.GetProperty('battery.reporting.unit')
-#print 'voltage current', dev_obj.GetProperty('battery.voltage.current')
-#print 'voltage design', dev_obj.GetProperty('battery.voltage.design')
-#print 'voltage unit', dev_obj.GetProperty('battery.voltage.unit')
+print 'charge current', dev_obj.GetProperty('battery.reporting.current')
+print 'charge design', dev_obj.GetProperty('battery.reporting.design')
+print 'charge last full',\
+    dev_obj.GetProperty('battery.reporting.last_full')
+print 'charge unit', dev_obj.GetProperty('battery.reporting.unit')
+print 'voltage current', dev_obj.GetProperty('battery.voltage.current')
+print 'voltage design', dev_obj.GetProperty('battery.voltage.design')
+print 'voltage unit', dev_obj.GetProperty('battery.voltage.unit')
     
