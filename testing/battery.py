c5d0840dd8b06e41543b3f596dfe1d16573d93e7
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-10-27 14:10:05 +0100

    Updated batteryinfo again

--- testing/battery.py
+++ testing/battery.py
@@ -8,10 +8,14 @@ dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
  
 #print 'charge level percentage',\
 #    dev_obj.GetProperty('battery.charge_level.percentage')
+print 'charge current', dev_obj.GetProperty('battery.reporting.current')
+print 'charge design', dev_obj.GetProperty('battery.reporting.design')
 battery_left = dev_obj.GetProperty('battery.reporting.current')
 battery_lifetime =dev_obj.GetProperty('battery.reporting.design')
+print "Battery left: ", battery_left
+print "Battery lifetime: ", battery_lifetime
 percentage_left = battery_left/battery_lifetime
-print "Battery left: ", percentage_left
+print "Battery left (%): ", percentage_left
 #print 'charge last full',\
 #    dev_obj.GetProperty('battery.reporting.last_full')
 #print 'charge unit', dev_obj.GetProperty('battery.reporting.unit')
