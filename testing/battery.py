efe4879bb02f3348b0d63941fca3de5fd021e3b8
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-10-27 14:22:23 +0100

    Updated batteryinfo again again again*2

--- testing/battery.py
+++ testing/battery.py
@@ -7,20 +7,13 @@ hal = dbus.Interface (hal_obj, 'org.freedesktop.Hal.Manager')
 uids = hal.FindDeviceByCapability('battery')
 dev_obj = bus.get_object ('org.freedesktop.Hal', uids[0])
  
-#print 'charge level percentage',\
-#    dev_obj.GetProperty('battery.charge_level.percentage')
-print 'charge current', dev_obj.GetProperty('battery.reporting.current')
-print 'charge design', dev_obj.GetProperty('battery.reporting.design')
 battery_left = dev_obj.GetProperty('battery.reporting.current')
 battery_lifetime =dev_obj.GetProperty('battery.reporting.design')
 print "Battery left: ", battery_left
 print "Battery lifetime: ", battery_lifetime
 percentage_left = battery_left*100/battery_lifetime
 print "Battery left (%): ", percentage_left
-#print 'charge last full',\
-#    dev_obj.GetProperty('battery.reporting.last_full')
-#print 'charge unit', dev_obj.GetProperty('battery.reporting.unit')
-#print 'voltage current', dev_obj.GetProperty('battery.voltage.current')
-#print 'voltage design', dev_obj.GetProperty('battery.voltage.design')
-#print 'voltage unit', dev_obj.GetProperty('battery.voltage.unit')
+
+
+
     
