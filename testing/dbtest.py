7ecca20f56cdcddad36ae2b368f5300736dba70d
FreddieBoi <frepe593@student.liu.se>
2009-10-28 16:38:57 +0100

    swedish characters management

--- testing/dbtest.py
+++ testing/dbtest.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import Column, Integer, Text, create_engine
+from sys import exit
+from sqlalchemy import Column, Integer, Text, UnicodeText, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 class Database(object):
     
     def __init__(self):
-        self.engine = create_engine('sqlite:///database.db', echo=True)
+        self.engine = create_engine('sqlite:///database.db', echo=False)
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
@@ -17,7 +18,7 @@ class MapObjectData(Base):
     id = Column(Integer, primary_key=True)
     coordx = Column(Integer)
     coordy = Column(Integer)
-    name = Column(Text)
+    name = Column(UnicodeText)
     timestamp = Column(Text)
 
     def __init__(self, coord, name, timestamp):
@@ -35,19 +36,16 @@ class MapObjectData(Base):
 if __name__ == '__main__':
     db = Database()
     Base.metadata.create_all(db.engine)
-    mod = MapObjectData((0,0), u"ää", "14:37")
+    mod = MapObjectData((0,0), u"hej", "14:37")
     db.session.add(mod)
-#    db.session.delete(db.session.query(MapObjectData).filter_by(name="ää").first())
+    crap = MapObjectData((1,1), u"åäö", "22:23")
+    db.session.add(crap)
+#    for instance in db.session.query(MapObjectData).filter_by(name="hej"):
+#        db.session.delete(instance)
     db.session.commit()
     for mod in db.session.query(MapObjectData):
-        print mod.name
+        print mod.name.encode("utf-8")
+    db.session.close()
+
 #    unitdata = UnitData((0,0), "unit", "00:00")
 #    db.session.add(unitdata)
-#    for mod in db.session.query(MapObjectData).order_by(MapObjectData.id):
-#        print mod
-#    print db.session.query(MapObjectData).filter_by(name="Waagh").first()
-#    print q
-#    mod.name = 'altered'
-#    print db.session.dirty
-#    db.session.commit()
-#    print db.session.dirty
