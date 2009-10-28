fbd2d7d453436e9f283bf85e2dc38d5a89571d9a
FreddieBoi <frepe593@student.liu.se>
2009-10-28 15:50:37 +0100

    db testing

--- testing/dbtest.py
+++ testing/dbtest.py
@@ -1,5 +1,5 @@
-#from sqlalchemy import *
-from sqlalchemy import Column, Integer, String, create_engine
+# -*- coding: utf-8 -*-
+from sqlalchemy import Column, Integer, Text, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,9 +9,6 @@ class Database(object):
         self.engine = create_engine('sqlite:///database.db', echo=True)
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
-        mod = MapObjectData((2,4), "Waagh", "13:37")
-        self.session.add(mod)
-        
 
 Base = declarative_base()
 class MapObjectData(Base):
@@ -20,8 +17,8 @@ class MapObjectData(Base):
     id = Column(Integer, primary_key=True)
     coordx = Column(Integer)
     coordy = Column(Integer)
-    name = Column(String)
-    timestamp = Column(String)
+    name = Column(Text)
+    timestamp = Column(Text)
 
     def __init__(self, coord, name, timestamp):
         self.coordx = coord[0]
@@ -32,12 +29,25 @@ class MapObjectData(Base):
     def __repr__(self):
         return "<%s: %s, %s, %s, %s>" % (self.__class__.__name__, self.coordx, self.coordy, self.name, self.timestamp)
 
+#class UnitData(MapObjectData):
+#    type = Column(String)
+
 if __name__ == '__main__':
     db = Database()
     Base.metadata.create_all(db.engine)
-    mod = db.session.query(MapObjectData).filter_by(name="Waagh").first()
-    print mod
-    mod.name = 'altered'
-    print db.session.dirty
+    mod = MapObjectData((0,0), u"ää", "14:37")
+    db.session.add(mod)
+#    db.session.delete(db.session.query(MapObjectData).filter_by(name="ää").first())
     db.session.commit()
-    print db.session.dirty
+    for mod in db.session.query(MapObjectData):
+        print mod.name
+#    unitdata = UnitData((0,0), "unit", "00:00")
+#    db.session.add(unitdata)
+#    for mod in db.session.query(MapObjectData).order_by(MapObjectData.id):
+#        print mod
+#    print db.session.query(MapObjectData).filter_by(name="Waagh").first()
+#    print q
+#    mod.name = 'altered'
+#    print db.session.dirty
+#    db.session.commit()
+#    print db.session.dirty
