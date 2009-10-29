f1954bbede4f60f97f4e189645bc315ecae68a9e
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-10-29 15:53:03 +0100

    Changed missionscreen to align_left and smaller font size for name and number

--- client/gui/missionscreen.py
+++ client/gui/missionscreen.py
@@ -6,6 +6,7 @@ import shared.data
 import pygtk
 pygtk.require('2.0')
 import gui
+import pango
 
 class MissionScreen(gtk.ScrolledWindow, gui.Screen):
     event_entry = None
@@ -17,8 +18,9 @@ class MissionScreen(gtk.ScrolledWindow, gui.Screen):
 
     def new_entry(self, labeltext):
         label = gtk.Label(labeltext)
+        label.set_alignment(0, 0.5)
         entry = gtk.Entry()
-        entry.set_max_length(100)
+        entry.set_max_length(300)
         entry.set_text("")
         entry.select_region(0, len(entry.get_text()))
         
@@ -29,12 +31,14 @@ class MissionScreen(gtk.ScrolledWindow, gui.Screen):
         gtk.ScrolledWindow.__init__(self)
         # create a new window
 
-        hbox = gtk.HBox(False,2)
+        hbox = gtk.HBox(False,0)
         self.add_with_viewport(hbox)
-        left_box = gtk.VBox(True,8)
-        right_box = gtk.VBox(True,8)
-        hbox.add(left_box)
+        left_box = gtk.VBox(True,0)
+        right_box = gtk.VBox(True,0)
+#        hbox.add(left_box)
+        hbox.pack_start(left_box,False,False,0)
         hbox.add(right_box)
+        
         
 
         label, self.event_entry = self.new_entry("Händelse")
@@ -49,16 +53,27 @@ class MissionScreen(gtk.ScrolledWindow, gui.Screen):
         left_box.add(label)
         right_box.add(self.hurted_entry)
         
+        
+        #SET A LABELTEXT TO BOLD!
+#        label = gtk.Label()
+#        # Pango markup should be on by default, if not use this line
+#        #label.set_use_markup(True)
+#        label.set_markup("<b>Look, I'm bold</b>")
+        
+        
         contact = gtk.Label("Kontaktperson:")
+        contact.set_alignment(0, 0.5)
         invisible_label = gtk.Label("")
         left_box.add(contact)
         right_box.add(invisible_label)
         
-        label, self.name_entry = self.new_entry("Namn")
+        label, self.name_entry = self.new_entry("      Namn")
+        label.modify_font(pango.FontDescription("sans 12"))
         left_box.add(label)
         right_box.add(self.name_entry)
         
-        label, self.number_entry = self.new_entry("Nummer")
+        label, self.number_entry = self.new_entry("      Nummer")
+        label.modify_font(pango.FontDescription("sans 12"))
         left_box.add(label)
         right_box.add(self.number_entry)
 
