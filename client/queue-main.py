53c2d7460ddc7d5f7aae7949ac05cf04a7817f59
Erik Eloff <eriel743@student.liu.se>
2009-10-30 13:41:54 +0100

    Added mainloop and demo text

--- client/queue-main.py
+++ client/queue-main.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
     q = queue.Queue()
     q.connect_to_server("localhost",50000)
 
+    q.mainloop()
+
--- client/queue.py
+++ client/queue.py
@@ -1,10 +1,12 @@
 import socket
 import threading
+import time
 
 class Queue(threading.Thread):
     output = []
     input = []
     socket = None
+    running = False
 
     def __init__(self):
         pass
@@ -15,3 +17,23 @@ class Queue(threading.Thread):
     def connect_to_server(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host,port))
+        self.running = True
+
+    def mainloop(self):
+        t = threading.Thread(target=self.listener)
+        t.start()
+        i = 0
+        while self.running:
+            self.socket.send("Hejsan" + str(i))
+            i+=1
+            time.sleep(1)
+
+        self.socket.close()
+
+    def listener(self):
+        while self.running:
+            data = self.socket.recv(1000)
+            print data
+            if data == "Hejsan10":
+                self.running = False
+
