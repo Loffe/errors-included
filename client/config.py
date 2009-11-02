a19cbf821549820e55e4008b59b7b9a7853e7c08
Erik Eloff <eriel743@student.liu.se>
2009-11-02 08:36:49 +0100

    Create config file for server connection

---
+++ client/config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    pass
+
+server = Config()
+server.ip = '127.0.0.1'
+server.port = 50000
--- client/queue-main.py
+++ client/queue-main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 import queue
+import config
 
 if __name__ == "__main__":
     q = queue.Queue()
-    q.connect_to_server("localhost",50000)
+    q.connect_to_server(config.server.ip,config.server.port)
 
     for i in range(10):
         q.enqueue("hejsan" + str(i))
