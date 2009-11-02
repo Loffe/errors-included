e63e8bc2c7bd8af33345743bb9eda53a13b2405c
Erik Eloff <eriel743@student.liu.se>
2009-11-02 11:06:36 +0100

    Implement dbus in queue. This breaks threading

--- client/queue.py
+++ client/queue.py
@@ -1,18 +1,22 @@
+import gobject
 import socket
 import threading
 import time
+import dbus
+import dbus.service
+import dbus.mainloop.glib
 
-class Queue(threading.Thread):
+class Queue(dbus.service.Object):
     output = []
     input = []
     socket = None
     running = False
 
-    def __init__(self):
-        pass
-
+    @dbus.service.method(dbus_interface='com.example.Queue',
+                         in_signature='v', out_signature='s')
     def enqueue(self, msg):
         self.output.append(msg)
+        return "message queued :)"
 
     def connect_to_server(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,8 +27,12 @@ class Queue(threading.Thread):
         self.socket.close()
 
     def mainloop(self):
-        t = threading.Thread(target=self.listener)
-        t.start()
+        #t = threading.Thread(target=self.listener)
+        #t.start()
+        #dbus_t = threading.Thread(target=self.dbus_main)
+        #dbus_t.start()
+        self.dbus_main()
+
         while self.running:
             if len(self.output) > 0:
                 msg = self.output.pop()
@@ -41,3 +49,14 @@ class Queue(threading.Thread):
                 continue
             self.input.append(data)
 
+
+    def dbus_main(self):
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+        session_bus = dbus.SessionBus()
+        name = dbus.service.BusName("com.example.Queue", session_bus)
+        object = Queue(session_bus, '/Queue')
+
+        mainloop = gobject.MainLoop()
+        print "Running example queue service."
+        mainloop.run()
