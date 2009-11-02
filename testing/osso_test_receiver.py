553be821b1c7c78a37ab6a27e9a554953879e8d9
Erik Eloff <eriel743@student.liu.se>
2009-11-02 09:12:55 +0100

    init osso in gtk.main()

--- testing/osso_test_receiver.py
+++ testing/osso_test_receiver.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python2.5
 import osso
 import gtk
+import gobject
 
 def callback_func(interface, method, arguments, user_data):
     print "RPC received"
     osso_c = user_data
     osso_c.system_note_infoprint("osso_test_receiver: Received a RPC to %s." % method)
 
-osso_c = osso.Context("osso_test_receiver", "0.0.1", False)
-print "osso_test_receiver started"
-rpc = osso.Rpc(osso_c)
-rpc.set_rpc_callback("spam.eggs.osso_test_receiver",
+def init():
+    print "init()"
+    osso_c = osso.Context("osso_test_receiver", "0.0.1", False)
+    print "osso_test_receiver started"
+    rpc = osso.Rpc(osso_c)
+    rpc.set_rpc_callback("spam.eggs.osso_test_receiver",
                             "/spam/eggs/osso_test_receiver",
                             "spam.eggs.osso_test_receiver", callback_func,
                             osso_c)
+print "before"
+gobject.idle_add(init)
+print "after"
 gtk.main()
