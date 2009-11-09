f82d17377e8045340fb3457a782b8595c3cd8971
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-11-09 14:47:03 +0100

    More testing about signal strength

--- testing/signalstrength.py
+++ testing/signalstrength.py
@@ -1,9 +1,78 @@
+#!/usr/bin/python2.5 
+
+"""
+Tests statistics functions
+
+Lauro Moura <lauro.neto@indt.org.br>, 2007
+"""
+
+import conic
 import dbus
-bus = dbus.SystemBus()
-eth0 = bus.get_object('org.freedesktop.NetworkManager',
-                      '/org/freedesktop/NetworkManager/Devices/eth0')
-eth0_dev_iface = dbus.Interface(eth0,
-    dbus_interface='org.freedesktop.NetworkManager.Devices')
-props = ewth0_dev_iface.getProperties()
-print props
-# props is the same as before
+import dbus.glib
+import gobject
+
+counter = 0
+loop = None
+iap_id = None
+
+def request_statistics(connection):
+    global counter, loop
+    print "request_statistics():"
+    
+    if counter >= 10:
+        print "Max counter reached (%i), quitting", counter
+        loop.quit()
+        return True
+        
+    counter += 1
+    connection.statistics(iap_id)
+    
+    return True    
+
+
+def statistics_cb(connection, event, data):
+    print "statistics(%s, %s, %x)" % (connection, event, data)
+    
+    print "time active=%i" % event.get_time_active()
+    print "signal_strength=%i" % event.get_signal_strength()
+    print "rx_packets=%u" % event.get_rx_packets()
+    print "tx_packets=%u" % event.get_tx_packets()
+    print "rx_bytes=%u" % event.get_rx_bytes()
+    print "tx_bytes=%u" % event.get_tx_bytes()
+
+
+def start():
+    print "start():"
+    connection = conic.Connection()
+    connection.connect("connection-event", connection_cb, 0xFFAA)
+    connection.connect("statistics", statistics_cb, 0x55AA)
+    connection.request_connection(conic.CONNECT_FLAG_NONE)
+    
+    return False
+
+    
+def connection_cb(connection, event, data):
+    global iap_id
+    
+    print "connection_cb(%s, %s, %x)" % (connection, event, data)
+
+    status = event.get_status()
+    error = event.get_error()
+    iap_id = event.get_iap_id()
+    bearer = event.get_bearer_type()
+    
+    if status == conic.STATUS_CONNECTED:
+        print "1: (CONNECTED (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
+        gobject.timeout_add(10000, request_statistics, connection)
+    elif status == conic.STATUS_DISCONNECTED:
+        print "1: (DISCONNECTED (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
+    elif status == conic.STATUS_DISCONNECTING:
+        print "1: (DISCONNECTING (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
+   
+
+if __name__ == "__main__":
+    loop = gobject.MainLoop()
+    bus = dbus.SystemBus(private=True)
+    
+    gobject.idle_add(start)
+    loop.run()
