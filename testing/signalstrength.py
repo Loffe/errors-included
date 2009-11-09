b6cee2ebae66fef6ff828fd17b79d23b2156eddb
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-11-09 15:17:39 +0100

    More testing SS

--- testing/signalstrength.py
+++ testing/signalstrength.py
@@ -31,49 +31,49 @@ def request_statistics(connection):
 
 
 def statistics_cb(connection, event, data):
-    print "laban"
-    print "statistics(%s, %s, %x)" % (connection, event, data)
+#    print "laban"
+#    print "statistics(%s, %s, %x)" % (connection, event, data)
     
-    print "time active=%i" % event.get_time_active()
+#    print "time active=%i" % event.get_time_active()
     print "signal_strength=%i" % event.get_signal_strength()
-    print "rx_packets=%u" % event.get_rx_packets()
-    print "tx_packets=%u" % event.get_tx_packets()
-    print "rx_bytes=%u" % event.get_rx_bytes()
-    print "tx_bytes=%u" % event.get_tx_bytes()
+#    print "rx_packets=%u" % event.get_rx_packets()
+#    print "tx_packets=%u" % event.get_tx_packets()
+#    print "rx_bytes=%u" % event.get_rx_bytes()
+#    print "tx_bytes=%u" % event.get_tx_bytes()
 
 
 def start():
     print "start():"
     connection = conic.Connection()
-    print "connection"
-    connection.connect("connection-event", connection_cb, 0xFFAA)
-    print "event"
+
+#    connection.connect("connection-event", connection_cb, 0xFFAA)
+
     connection.connect("statistics", statistics_cb, 0x55AA)
-    print "stats"
+
     connection.request_connection(conic.CONNECT_FLAG_NONE)
-    print "request stats"
+
     request_statistics(connection)
     return False
 
     
-def connection_cb(connection, event, data):
-    global iap_id
-    
-    print "connection_cb(%s, %s, %x)" % (connection, event, data)
-
-    status = event.get_status()
-    error = event.get_error()
-    iap_id = event.get_iap_id()
-    bearer = event.get_bearer_type()
-    
-    if status == conic.STATUS_CONNECTED:
-        print "1: (CONNECTED (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
-        gobject.timeout_add(10000, request_statistics, connection)
-    elif status == conic.STATUS_DISCONNECTED:
-        print "1: (DISCONNECTED (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
-    elif status == conic.STATUS_DISCONNECTING:
-        print "1: (DISCONNECTING (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
-   
+#def connection_cb(connection, event, data):
+#    global iap_id
+#    
+#    print "connection_cb(%s, %s, %x)" % (connection, event, data)
+#
+#    status = event.get_status()
+#    error = event.get_error()
+#    iap_id = event.get_iap_id()
+#    bearer = event.get_bearer_type()
+#    
+#    if status == conic.STATUS_CONNECTED:
+#        print "1: (CONNECTED (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
+#        gobject.timeout_add(10000, request_statistics, connection)
+#    elif status == conic.STATUS_DISCONNECTED:
+#        print "1: (DISCONNECTED (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
+#    elif status == conic.STATUS_DISCONNECTING:
+#        print "1: (DISCONNECTING (%s, %s, %i, %i)" % (iap_id, bearer, status, error)
+#   
 
 if __name__ == "__main__":
     loop = gobject.MainLoop()
