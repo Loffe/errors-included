62e629dfad403ac7826df04e9b70225d59f4df57
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-11-12 10:56:58 +0100

    Testing vvoip_linus

--- testing/vvoip_linus.py
+++ testing/vvoip_linus.py
@@ -32,7 +32,7 @@ class GTK_Main:
         window.show_all()
 
         # Set up the gstreamer pipeline
-        self.player = gst.parse_launch ("v4l2src ! autovideosink")
+        self.player = gst.parse_launch ("v412src ! autovideosink")
 
         bus = self.player.get_bus()
         bus.add_signal_watch()
