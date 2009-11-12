75d2d0d9509f543ee910633a33ec7e5ed5c8d3cd
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-11-12 13:58:33 +0100

    testing stream to anoter device

--- testing/vvoip_linus.py
+++ testing/vvoip_linus.py
@@ -38,7 +38,11 @@ class GTK_Main:
 #        gst-launch v4l2src ! video/x-raw-yuv,width=352,height=288,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=<other N800's ip> port=5434 
 
         #Stream from another device
-        self.player = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=320,height=240,framerate=20/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.219.107 port=5434")
+#        self.player = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=320,height=240,framerate=8/1 ! hantro4200enc ! rtph263pay ! udpsink host=130.236.219.107 port=5434")
+        
+        self.player = gst.parse_launch("v4l2src ! video/x-raw-yuv,width=176,height=144,framerate=\(fraction\)15/1 ! hantro4200enc stream-type=1 profile-and-level=1001 !video/x-h263,framerate=\(fraction\)15/1 ! rtph263ppay mtu=1438 ! udpsink host=130.236.219.107 port=5434 dsppcmsrc ! queue ! audio/x-raw-int,channels=1,rate=8000 ! mulawenc ! rtppcmupay mtu=1438 ! udpsink host=130.236.219.107 port=5432")
+
+        
         # Show my webcam
 #        self.player = gst.parse_launch ("v4l2src ! video/x-raw-yuv, width=320, height=240, framerate=8/1 ! autovideosink")
 
