716b36c3b13087ba133d97e352b0bf88f10bb8dc
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-11-16 10:56:34 +0100

    Testing videocall

--- client/gui/camerascreen.py
+++ client/gui/camerascreen.py
@@ -15,12 +15,13 @@ class CamScreen(gtk.ScrolledWindow, gui.Screen):
     def __init__(self,db):
         # set automatic horizontal and vertical scrolling
         self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        window.set_title("Webcam-Viewer")
-        window.set_default_size(500, 400)
-        window.connect("destroy", gtk.main_quit, "WM destroy")
+        gtk.ScrolledWindow.__init__(self)
+#        window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+#        window.set_title("Webcam-Viewer")
+#        window.set_default_size(500, 400)
+#        window.connect("destroy", gtk.main_quit, "WM destroy")
         vbox = gtk.VBox()
-        window.add(vbox)
+        self.add_with_viewport(vbox)
         self.movie_window = gtk.DrawingArea()
         vbox.add(self.movie_window)
         hbox = gtk.HBox()
@@ -30,11 +31,11 @@ class CamScreen(gtk.ScrolledWindow, gui.Screen):
         self.button = gtk.Button("Start")
         self.button.connect("clicked", self.start_stop)
         hbox.pack_start(self.button, False)
-        self.button2 = gtk.Button("Quit")
-        self.button2.connect("clicked", self.exit)
-        hbox.pack_start(self.button2, False)
+        #self.button2 = gtk.Button("Quit")
+        #self.button2.connect("clicked", self.exit)
+       # hbox.pack_start(self.button2, False)
         hbox.add(gtk.Label())
-        window.show_all()
+        self.show_all()
 
         #Listening for Input:
 #        gst-launch udpsrc port=5434 caps=application/x-rtp,clock-rate=90000 ! rtph263depay ! hantro4100dec ! xvimagesink
