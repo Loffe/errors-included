88a3c478c126accd387c0a0f790c05ebc1a94fd1
Martin Kristing <markr471@sysi-04.sysinst.ida.liu.se>
2009-11-19 11:58:18 +0100

    added the new screens

---
+++ client/gui/statusscreen.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import gtk
+import gobject
+import map.mapdata
+import shared.data
+import datetime
+import gui
+import pango
+
+class StatusScreen(gtk.ScrolledWindow, gui.Screen):
+    '''
+    The screen in which you create a new alarm.
+    '''
+    # the entries
+    event_entry = None
+    location_entry = None
+    location_entry2 = None
+    location_entry3 = None
+    hurted_entry = None
+    name_entry = None
+    number_entry = None
+    random_entry = None
+    db = None
+
+    def __init__(self, db):
+        '''
+        Constructor. Create the statusscreen and its entries.
+        '''
+        gtk.ScrolledWindow.__init__(self)
+        self.db = db 
