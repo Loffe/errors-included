79548cd0c1d5c0f22350061e7fd586a41cd56fcd
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-11-20 09:27:55 +0100

    Testing audiovideo

--- client/clientgui.py
+++ client/clientgui.py
@@ -381,15 +381,15 @@ class ClientGui(hildon.Program):
     def show_cam(self, event):
 #        self.screens["camera"].start_video_send(self.screens["contact"].ip)
         self.screens["camera"].start_audio_recv(5432)
-        self.screens["camera"].start_audio_send(self.screens["contact"].ip, 5432)
-        self.screens["camera"].start_video_send(self.screens["contact"].ip, 5434)
+        self.screens["camera"].start_audio_send(self.screens["contact"].ip2, 5432)
+        self.screens["camera"].start_video_send(self.screens["contact"].ip2, 5434)
         self.screens["camera"].start_video_recv(5434)
         self.show(["camera"])
         
     def show_voice(self, event):
 #        self.screens["camera"].start_video_send(self.screens["contact"].ip)
         self.screens["camera"].start_audio_recv(5432)
-        self.screens["camera"].start_audio_send(self.screens["contact"].ip, 5432)
+        self.screens["camera"].start_audio_send(self.screens["contact"].ip2, 5432)
         self.show(["camera"])
         
     def show_outbox(self, event):
