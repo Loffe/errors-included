c774f3d75226f71ffa759e09f1519dd5458f2439
Erik Eloff <eriel743@student.liu.se>
2009-11-25 09:03:22 +0100

    Deafault users with small caps

--- server/main.py
+++ server/main.py
@@ -60,9 +60,9 @@ if __name__ == "__main__":
         session = db._Session()
 
         # Create users
-        u = User(u"Ragnar", u"prydlig frisyr")
+        u = User(u"ragnar", u"prydlig frisyr")
         session.add(u)
-        u = User(u"Slanggurka", u"smakar som nors")
+        u = User(u"slanggurka", u"smakar som nors")
         session.add(u)
 
         # Create units
