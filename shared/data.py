2a98e766701bce801043cedef9d5c7a4311615b2
Erik Eloff <eriel743@student.liu.se>
2009-11-26 10:04:26 +0100

    Removed trailing bugging ,

--- shared/data.py
+++ shared/data.py
@@ -171,7 +171,7 @@ class ObjectID(Base):
             return repr
 
 class UnitType(object):
-    ambulance = u"ambulance", # Regular unit
+    ambulance = u"ambulance" # Regular unit
     army = u"army" # short for Swedish Armed Forces
     commander = u"commander" # Nana nana nana nana LEADER! leader..
     srsa = u"srsa" # Swedish Rescue Services Agency (SRSA)
