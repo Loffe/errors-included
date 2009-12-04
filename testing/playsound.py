7776afbb727e81f5d1224881d38d6c821907c9de
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-12-04 14:01:44 +0100

    Testing play sound

--- testing/playsound.py
+++ testing/playsound.py
@@ -9,6 +9,6 @@ def play_uri(uri):
     print 'Playing:', uri
     player.set_property('uri', uri)
     player.set_state(gst.STATE_PLAYING)
-    play_uri("/home/user/tada.wav")
     
+play_uri("snd/mail3b.wav")
 gtk.main()
