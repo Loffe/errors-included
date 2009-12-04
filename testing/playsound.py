41c7d1edc1d72225af0e5b706cb42568ac29f3d7
Linus Flod <linfl683@sysi-07.sysinst.ida.liu.se>
2009-12-04 14:00:47 +0100

    Testing play sound

---
+++ testing/playsound.py
@@ -0,0 +1,14 @@
+import pygst
+pygst.require('0.10')
+import gst
+import gobject, sys
+import gtk
+
+def play_uri(uri):
+    player = gst.element_factory_make("playbin", "player")
+    print 'Playing:', uri
+    player.set_property('uri', uri)
+    player.set_state(gst.STATE_PLAYING)
+    play_uri("/home/user/tada.wav")
+    
+gtk.main()
