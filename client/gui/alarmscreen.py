62149dda376f1534ae32c98e059c63671b40c693
FreddieBoi <frepe593@student.liu.se>
2009-12-08 11:59:47 +0100

    added selectable event types in alarm creation and don't let people change alarms (or their POIs)

--- client/gui/alarmscreen.py
+++ client/gui/alarmscreen.py
@@ -6,7 +6,7 @@ import map.mapdata
 import shared.data
 import datetime
 import gui
-
+import pango
 
 class AlarmScreen(gtk.ScrolledWindow, gui.Screen):
     '''
@@ -32,6 +32,24 @@ class AlarmScreen(gtk.ScrolledWindow, gui.Screen):
         right_box = gtk.VBox(True,0)
         hbox.pack_start(left_box,False,False,0)
         hbox.pack_start(right_box)
+        
+        # create type label
+        type_label = gtk.Label("Typ:")
+        type_label.modify_font(pango.FontDescription("sans 12"))
+        type_label.set_alignment(0, 0.5)
+        left_box.pack_start(type_label, True, True, 0)
+        
+        # create combobox used to select event type (poi.subtype)
+        self.combo_box = gtk.combo_box_new_text()
+        self.selected_type = None
+        right_box.pack_start(self.combo_box, True, False, 0)
+        # add event handler
+        self.combo_box.connect('changed', self.select_type)
+        # add selectable types
+        types = ["accident", "fire", "other"]
+        for type in types:
+            self.combo_box.append_text(type)
+        self.combo_box.set_active(0)
 
         # create entries
         self.event_entry = self.new_entry("Händelse:", left_box, right_box)
@@ -53,7 +71,15 @@ class AlarmScreen(gtk.ScrolledWindow, gui.Screen):
         self.other_entry = self.new_entry("Övrigt:", left_box, right_box)
 
         self.show_all()
-
+        
+    def select_type(self, combobox):
+        '''
+        Call when combobox changes to switch obstacle type.
+        @param combobox: the changed combobox
+        '''
+        # set the selected type
+        self.selected_type = unicode(combobox.get_active_text())
+        
     def ok_button_function(self, event):
         lon = float(self.coordx_entry.get_text())
         lat = float(self.coordy_entry.get_text())
@@ -61,7 +87,8 @@ class AlarmScreen(gtk.ScrolledWindow, gui.Screen):
                 lon,lat,
                 unicode(self.event_entry.get_text()),
                 datetime.datetime.now(),
-                shared.data.POIType.flag)
+                shared.data.POIType.event,
+                self.selected_type)
         self.db.add(poi_data)
 
         alarm = shared.data.Alarm(
